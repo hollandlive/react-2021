7349c9e902e63bde65dfff04762526d9b4429ce6
Artemis A <dutchnewsru@gmail.com>
2021-11-30 16:35:22 +0100

    added component homePage

---
+++ dutchnews-react/packages/mars-theme/src/components/homePage/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Container = styled.div`
+  width: 800px;
+  margin: 0;
+  padding: 24px;
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  margin-top: 24px;
+  margin-bottom: 8px;
+  color: rgba(12, 17, 43);
+`;
+
+export function HomePage(props) {
+
+}

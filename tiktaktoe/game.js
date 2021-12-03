cca9ceba323a80b0156441d4624b881dc689e593
Artemis A <dutchnewsru@gmail.com>
2021-12-03 11:44:50 +0100

    js logic begins

--- tiktaktoe/game.js
+++ tiktaktoe/game.js
@@ -1,13 +1,6 @@
-//console.log("Helo world!");
-
 //script is before body -> check if HTML elements are fully loaded and if condition "reset button" has been clicked 
 
-// ES 5
-
-window.addEventListener('DOMContentLoaded', function() {
-  alert("Hello World!");
-});
-// ES 6 
-//window.addEventListener('DOMContentLoaded', ("click") => {
-//    console.log('DOM fully loaded and parsed');
-//};
+window.addEventListener('DOMContentLoaded', () => {
+            // handle the click event
+            console.log('DOM fully loaded and parsed');
+        });

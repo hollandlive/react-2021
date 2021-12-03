480d214e96596ce0b21764b86d743383f99b7c64
Artemis A <dutchnewsru@gmail.com>
2021-12-03 12:10:15 +0100

    delete

--- tiktaktoe/game.js
+++
@@ -1,6 +0,0 @@
-//script is before body -> check if HTML elements are fully loaded and if condition "reset button" has been clicked 
-
-window.addEventListener('DOMContentLoaded', () => {
-            // handle the click event
-            console.log('DOM fully loaded and parsed');
-        });

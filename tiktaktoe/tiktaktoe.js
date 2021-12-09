a4faf3398a87e48d7a1d53a85339acd4eed3d878
Artemis A <dutchnewsru@gmail.com>
2021-12-09 13:06:14 +0100

    change

--- tiktaktoe/tiktaktoe.js
+++ tiktaktoe/tiktaktoe.js
@@ -1,1 +1,26 @@
-document.getElementById('reset');
+// eloquent JS - lesson 1 exersises
+
+// var length = 7;
+
+// console.log('#');
+// console.log('##');
+// console.log('###');
+// console.log('####');
+// console.log('#####');
+// console.log('######');
+// console.log('#######');
+
+let text = "#";
+
+for (let i = 0; i < 5; i++) {
+  text += "The number is " + text + "<br>";
+  console.log(text);
+}
+
+
+
+
+
+
+
+

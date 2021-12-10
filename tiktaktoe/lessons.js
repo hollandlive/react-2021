72039495e135831c7c728f55dd8b8d8c44414375
Artemis A <dutchnewsru@gmail.com>
2021-12-10 12:54:49 +0100

    lessons.js separated

---
+++ tiktaktoe/lessons.js
@@ -0,0 +1,18 @@
+// eloquent JS - lesson 1 exersises
+
+// var length = 7;
+
+// console.log('#');
+// console.log('##');
+// console.log('###');
+// console.log('####');
+// console.log('#####');
+// console.log('######');
+// console.log('#######');
+
+let text = "#";
+
+for (let i = 0; i < 5; i++) {
+  text = text + text;
+  console.log(text);
+}

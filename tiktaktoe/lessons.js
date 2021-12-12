9590a7fa2ac59d8b927d934fab710b59220868a2
Artemis A <dutchnewsru@gmail.com>
2021-12-12 12:00:59 +0100

    change

--- tiktaktoe/lessons.js
+++ tiktaktoe/lessons.js
@@ -16,3 +16,13 @@ for (let i = 0; i < 5; i++) {
   text = text + text;
   console.log(text);
 }
+
+let lastName = 'aksen';
+let firstName = 'artem';
+let email = 'art@aksen.com';
+
+
+fullName = lastName + '' + firstName;
+
+console.log(fullName.length);
+console.log(email.indexOf('@'));
--- tiktaktoe/tiktaktoe.js
+++ tiktaktoe/tiktaktoe.js
@@ -1,4 +1,25 @@
+// add event listener on html (DOM) loaded and parsed because JS is in <head>
+// Example from developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM fully loaded and parsed');
+  
+  const reset = document.querySelector('#reset');
+  const player_x = document.querySelector('#player_x');
+  const player_0 = document.querySelector('#player_0');
 
+
+  reset.addEventListener('click', () => {
+    console.log('preseed');
+  });
+
+  player_x.addEventListener('click', () => {
+    console.log('player_x');
+  });
+
+  player_0.addEventListener('click', () => {
+    console.log('player_0');
+  });
+});
 
 
 

048f5cec11c821ef8f98083f63e223d709a6cbcc
Artemis A <dutchnewsru@gmail.com>
2021-12-14 15:57:14 +0100

    JS looping through cells function clicked once

--- tiktaktoe/lessons.js
+++ tiktaktoe/lessons.js
@@ -4,14 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const restart = document.querySelector('#restart');
   const cells = document.querySelectorAll('[data-cell]')
 
+  // looping through cells - if clicked ONCE only - check it
   cells.forEach(cell => {
-    cell.addEventListener('click', cellClicked)
+    cell.addEventListener('click', cellClicked, {once : true})
   })
 
   function cellClicked () {
     console.log('clicccked');
   }
 
+  // test restart button
   restart.addEventListener('click', () => {
     console.log('Game to Restart');
   });

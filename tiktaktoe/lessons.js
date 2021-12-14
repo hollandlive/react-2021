abe6c59ac7bba8ca648a54fce07bdb02b8cefb5b
Artemis A <dutchnewsru@gmail.com>
2021-12-14 15:19:00 +0100

    started JS script for the game

--- tiktaktoe/lessons.js
+++ tiktaktoe/lessons.js
@@ -1,3 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM fully loaded and parsed');
+  
+  const restart = document.querySelector('#restart');
+  const cells = document.querySelectorAll('[data-cell]')
+
+  cells.forEach(cell => {
+    cell.addEventListener('click', cellClicked)
+  })
+
+  function cellClicked () {
+    console.log('clicccked');
+  }
+
+  restart.addEventListener('click', () => {
+    console.log('Game to Restart');
+  });
+});
+
+
+
+
+
 // eloquent JS - lesson 1 exersises
 
 // var length = 7;
@@ -10,19 +33,19 @@
 // console.log('######');
 // console.log('#######');
 
-let text = "#";
+// let text = "#";
 
-for (let i = 0; i < 5; i++) {
-  text = text + text;
-  console.log(text);
-}
+// for (let i = 0; i < 5; i++) {
+//   text = text + text;
+//   console.log(text);
+// }
 
-let lastName = 'aksen';
-let firstName = 'artem';
-let email = 'art@aksen.com';
+// let lastName = 'aksen';
+// let firstName = 'artem';
+// let email = 'art@aksen.com';
 
 
-fullName = lastName + '' + firstName;
+// fullName = lastName + '' + firstName;
 
-console.log(fullName.length);
-console.log(email.indexOf('@'));
+// console.log(fullName.length);
+// console.log(email.indexOf('@'));

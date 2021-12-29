1db286c040d499027b534880222282b65ceee81e
Artemis A <dutchnewsru@gmail.com>
2021-12-29 08:27:45 +0100

    change

--- tiktaktoe/lessons.js
+++ tiktaktoe/lessons.js
@@ -1,22 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOM fully loaded and parsed');
-  
+
   const restart = document.querySelector('#restart');
-  const cells = document.querySelectorAll('[data-cell]')
+  const cells = document.querySelectorAll('[data-cell]');
+
+  const activeCell = false;
+
+  let gameBoard =
+    ['x', '0', 'x',
+      'x', '0', 'x',
+      '0', 'x', '0'];
 
   // looping through cells - if clicked ONCE only - check it
   cells.forEach(cell => {
-    cell.addEventListener('click', cellClicked, {once : true})
+    cell.addEventListener('click', cellClicked, { once: true });
+    function cellClicked() {
+
+      let isActive = 'player_0';
+
+      let render = cell.classList.add(isActive);
+    }
   })
 
-  function cellClicked () {
-    console.log('clicccked');
+  class Game {
+    constructor() {
+
+    }
   }
+
+
 
   // test restart button
   restart.addEventListener('click', () => {
     console.log('Game to Restart');
   });
+
+  let board = [];
 });
 
 

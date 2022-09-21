94a5203e6b8d43dcf177d3965493788b0b559c0b
Artemis A <dutchnewsru@gmail.com>
2022-09-21 15:57:00 +0200

    game new logic

--- game/game.js
+++ game/game.js
@@ -9,15 +9,37 @@ document.addEventListener('DOMContentLoaded', (event) => {
         3, 4, 5,
         6, 7, 8
     ]
-    cells[8] = 9;
-    console.log(cells[8]);
+
+
+    const winningCondtitions = [
+        012, 345,
+        678, 036,
+        147, 258,
+        048, 246
+    ]
+    // array.push - array.pop disruptive methods - it alters the initial value of a variable
+    // push - adds at the end, pop - distracts the last one
+
+    // let result = cells.push(11);
+    // result = cells.pop();
+
+    // let result = cells.join('');
+
+    let playerTurn = Math.floor(Math.random() * cells.length);
+
+    console.log(cells);
+    // console.log(result);
+    console.log(playerTurn, cells[playerTurn]);
+
+    //result was initially defined - but we can remove 'let' and redifine it
+
 
 
     // let html = `
     // <h2>the blog $(author)</h2>
     // <p>Hello $(title)</p>
     // `;
-    // console.log(html);
+    // console.log(html); - template string
 });
 
 

83776a13b8af18b02c08a01d1517b3b904627cd2
Artemis A <dutchnewsru@gmail.com>
2024-02-06 21:07:33 +0100

    change

--- scissors/scissors.js
+++ scissors/scissors.js
@@ -1,2 +1,18 @@
-document.getElementById('colod');
-console.log('colod');
+// start = document.getElementById('Start');
+// start.addEventListener("click", (e) => {
+//     console.log('nnn');
+// });
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     console.log('DOM fully loaded and parsedd')
+// })
+
+
+const btn = document.querySelector('#btn');
+const playerChoice = document.querySelector('#playerChoice');
+btn.onclick = (event) => {
+    e.preventDefault();
+    // show the selected index
+    alert(playerChoice.selectedIndex);
+};
+

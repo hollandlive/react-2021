033f18e0b07335efc271a0ddf0a64be3d71446b3
Artemis A <dutchnewsru@gmail.com>
2025-01-07 12:17:12 +0200

    js

--- fullstack-2025/fullstack.js
+++ fullstack-2025/fullstack.js
@@ -1,10 +1,59 @@
-const name = 'John';
-const age = 30;
+// const name = 'John';
+// const age = 30;
 
-// concatenation
-const concatenationGreeting = 'Hello! My name is ' + name + ' and I am ' + age + '.';
-console.log(concatenationGreeting); // 'Hello! My name is John and I am 30.'
+// // concatenation
+// const concatenationGreeting = 'Hello! My name is ' + name + ' and I am ' + age + '.';
+// console.log(concatenationGreeting); // 'Hello! My name is John and I am 30.'
 
-// interploation
-const interpolationGreeting = `Hello! My name is ${name} and I am ${age}.`;
-console.log(interpolationGreeting); // 'Hello! My name is John and I am 30.'
+// // interploation
+// const interpolationGreeting = `Hello! My name is ${name} and I am ${age}.`;
+// console.log(interpolationGreeting); // 'Hello! My name is John and I am 30.'
+
+// let goal = 'Help 1 million people worldwide build their careers in IT.';
+// // write your code below
+// console.log(goal.length);
+
+
+let goal = 'Help 1 million people worldwide build their careers in IT.';
+// write your code below
+console.log(
+    goal[goal.length - 1]
+);
+
+const name = 'Artem';
+const lastName = 'Aksenovs';
+const initials = name[0] + lastName[0];
+console.log(initials);
+//  Write code here
+
+const a = 'Hello';
+const b = 'world';
+// write your code below
+const resultString = `${a}, ${b}!`;
+console.log(resultString);
+
+const part1 = '2365';
+const part2 = '7341';
+const part3 = '3219';
+const part4 = '3348';
+// write your code below
+const cardNumber = `${part1} ${part2} ${part3} ${part4}`;
+console.log(cardNumber);
+
+let cash = 50;
+let milkPrice = 40;
+let canBuyMilk = cash >= milkPrice;
+console.log(canBuyMilk);
+
+const age = 58;
+const retirementAge = 65;
+const isRetired = age >= retirementAge;
+console.log(isRetired);
+
+let mark = 85;
+let passedTheTest = mark >= 60;
+
+let mustTakeTheTestAgain = !passedTheTest;
+
+console.log(mustTakeTheTestAgain);
+

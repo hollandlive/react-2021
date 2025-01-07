86730d0ef12981aa53a4fa28310281d902489bbc
Artemis A <dutchnewsru@gmail.com>
2025-01-07 11:24:51 +0200

    js

---
+++ fullstack-2025/fullstack.js
@@ -0,0 +1,10 @@
+const name = 'John';
+const age = 30;
+
+// concatenation
+const concatenationGreeting = 'Hello! My name is ' + name + ' and I am ' + age + '.';
+console.log(concatenationGreeting); // 'Hello! My name is John and I am 30.'
+
+// interploation
+const interpolationGreeting = `Hello! My name is ${name} and I am ${age}.`;
+console.log(interpolationGreeting); // 'Hello! My name is John and I am 30.'

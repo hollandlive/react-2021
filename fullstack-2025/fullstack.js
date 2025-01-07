a173f5794e3367304c1a8508a4da0c88ebffa57f
Artemis A <dutchnewsru@gmail.com>
2025-01-07 15:56:50 +0200

    js

--- fullstack-2025/fullstack.js
+++ fullstack-2025/fullstack.js
@@ -5,13 +5,13 @@
 // const concatenationGreeting = 'Hello! My name is ' + name + ' and I am ' + age + '.';
 // console.log(concatenationGreeting); // 'Hello! My name is John and I am 30.'
 
-// // interploation
-// const interpolationGreeting = `Hello! My name is ${name} and I am ${age}.`;
-// console.log(interpolationGreeting); // 'Hello! My name is John and I am 30.'
+// interploation
+const interpolationGreeting = `Hello! My name is ${name} and I am ${age}.`;
+console.log(interpolationGreeting); // 'Hello! My name is John and I am 30.'
 
-// let goal = 'Help 1 million people worldwide build their careers in IT.';
-// // write your code below
-// console.log(goal.length);
+let goal = 'Help 1 million people worldwide build their careers in IT.';
+// write your code below
+console.log(goal.length);
 
 
 let goal = 'Help 1 million people worldwide build their careers in IT.';
@@ -44,16 +44,83 @@ let cash = 50;
 let milkPrice = 40;
 let canBuyMilk = cash >= milkPrice;
 console.log(canBuyMilk);
-
+//
 const age = 58;
 const retirementAge = 65;
 const isRetired = age >= retirementAge;
 console.log(isRetired);
-
+//
 let mark = 85;
 let passedTheTest = mark >= 60;
-
 let mustTakeTheTestAgain = !passedTheTest;
-
 console.log(mustTakeTheTestAgain);
+//
+const isHoliday = true;
+const isVacation = false;
+const canStayHome = isHoliday || isVacation; // don't change this line
+console.log(canStayHome);
+//
+const hasAllIngredients = true;
+const hasStove = true;
+let canMakeCake = hasAllIngredients && hasStove;
+console.log(canMakeCake);
 
+let workDone = true;
+let dayFinished = true;
+//  Write code here
+let canGoHome = workDone || dayFinished;
+console.log(canGoHome);
+//
+function calculateArea(length, width) {
+    return length * width;
+}
+calculateArea(8, 9)
+calculateArea(3, 5);
+console.log(calculateArea);
+//
+function greet(name) {
+    return `Hi, ${name}!`;
+}
+greet('Mike');
+greet('John');
+console.log(greet);
+//
+function getBoxVolume(length, width, height = 1) {
+    let getVolume = length * width * height;
+    return getVolume;
+}
+getBoxVolume(3, 4, 5);
+getBoxVolume(5, 10)
+console.log(getBoxVolume);
+//
+function getMoneyRemaining(bankAccount, banknoteDenomination = 100) {
+    let remainder = bankAccount % banknoteDenomination;
+    return remainder;
+}
+getMoneyRemaining(1473, 20);
+getMoneyRemaining(1250, 50);
+getMoneyRemaining(534);
+console.log(getMoneyRemaining);
+//
+function getInitials(name, lastName) {
+    let initials = name[0] + lastName[0];
+    return initials;
+}
+getInitials('Alice', 'Green');
+getInitials('Bob', 'Smith');
+console.log(getInitials);
+//
+function greetIfKid(age) {
+    // write your code here
+    if (age < 18) {
+        age = 17;
+        console.log('Hey!');
+    }
+}
+//
+function greetIfRetired(age) {
+    if (age >= 65) {
+        age = 67;
+        console.log('Have a nice day!');
+    }
+}

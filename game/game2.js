d60571391d403a6933d56a0eab3c529c8cc2820d
Artemis A <dutchnewsru@gmail.com>
2025-01-11 13:50:52 +0200

    js

--- game/game2.js
+++ game/game2.js
@@ -1,10 +1,16 @@
 let gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-let computerPlay = randomNumber(gameBoard);
-let humanPlay = humanNumber(gameBoard);
-let decreasedGameBoard(gameBoard) = [];
+// let computerPlay = randomNumber(gameBoard);
+// let humanPlay = humanNumber(gameBoard);
+// let decreasedGameBoard = decreasedNumbers(gameBoard) = [];
 
 function randomNumber(gameBoard) {
 
+    random = Math.random(gameBoard);
+    return random;
 }
+random = randomNumber(gameBoard);
+console.log(random);
+
+
 
 

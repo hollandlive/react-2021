9714d90cb6ca4db2149dd6085958f5348f8f72e6
Artemis A <dutchnewsru@gmail.com>
2025-01-21 12:24:02 +0200

    checj

--- fullstack-2025/myFunctionsExamples2.js
+++ fullstack-2025/myFunctionsExamples2.js
@@ -299,3 +299,46 @@ function compareRobots(firstRobotResults, secondRobotResults) {
 }
 res1111 = compareRobots([9, 7, 9], [1, 3, 4, 5, 12, 17]);
 console.log(res1111);
+/* Get Location
+We're upgrading our robots! They can now convert motion commands into signals and move accordingly:
+
+'forward' translates to y + 1.
+'back' to y - 1.
+'right' to x + 1.
+'left' to x - 1.
+But it would be great if the robot could track its location without GPS. So, let's create the getLocation function that accepts two parameters:
+coordinates — an array of initial coordinates in the [x, y] format.
+commands — an array with a history of commands like ['command1', 'command2', 'command3', ...].
+The function should return an array of final coordinates ([x, y]) after moving according per commands.
+
+Examples:
+For coordinates = [2, 1] and commands = ['left', 'back', 'back']:
+Coordinates after the first command are: [1, 1] (1 step left).
+After the second command — [1, 0] (1 step back).
+After the third command — [1, -1] (1 step back).
+The result is the [1, -1] array.
+Or, in code:
+getLocation([0, 0], ['forward', 'right']); // [1, 1]
+getLocation([2, 3], ['back', 'back', 'back', 'right']); // [3, 0]
+getLocation([0, 5], ['back', 'back', 'back', 'right', 'left', 'forward']); // [0, 3] */
+function getLocation(coordinates, commands) {
+    let x = coordinates[0];
+    let y = coordinates[1];
+    for (let i = 0; i < commands.length; i++) {
+        if (commands[i] === 'left') {
+            x = x - 1;
+        } else if (commands[i] === 'right') {
+            x = x + 1;
+        } else if (commands[i] === 'forward') {
+            y = y + 1;
+        } else if (commands[i] === 'back') {
+            y = y - 1;
+        } else {
+            return 'wrong command';
+        }
+
+    }
+    return [x, y];
+}
+let coordinates = [10, 20];
+console.log(getLocation(coordinates, ['st', 'ss']))

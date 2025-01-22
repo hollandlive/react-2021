2bc3300dbd57f0ceabc745ade632d05fa09bcee8
Artemis A <dutchnewsru@gmail.com>
2025-01-22 09:21:45 +0200

    criss cross game logic

---
+++ fullstack-2025/jsAdvanced.js
@@ -0,0 +1,19 @@
+/*refactor program
+Format the program by following these rules:
+
+Each variable should be defined in a new line using a let or const keyword.
+Modify variable names so that they follow the camelCase style. Make sure they are short and meaningful, e.g.:
+nameofaperson --> userName;
+neweventgetfromserver --> newEvent;
+thedateofthenewevent --> eventDate;
+eventtextfrommessage --> eventMessage.
+Place a single space before and after =;
+Make sure all variables are properly formatted, e.g.:
+elonmusk --> Elon Musk;
+сreatedAnewCar --> created a new car;
+2019,10,10 --> 2019.10.10.*/
+const userName = 'Elon Musk';
+const newEvent = 'created a new car';
+const eventDate = '2019.10.10';
+const eventMessage = `${userName} ${newEvent} in ${eventDate}`;
+
